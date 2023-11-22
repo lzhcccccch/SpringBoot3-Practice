a90177bfa5414648426e5ec839ad344183817459
liuzhichao <liuzhichao00@163.com>
2023-11-22 14:10:28 +0800

    update: easyRule 补充链式编程用法

--- EasyRuleDemo/src/main/java/com/lzhch/practice/easyruledemo/local/LocalRuleApplication.java
+++ EasyRuleDemo/src/main/java/com/lzhch/practice/easyruledemo/local/LocalRuleApplication.java
@@ -5,6 +5,7 @@ import com.lzhch.practice.easyruledemo.rules.MyRuleThree;
 import com.lzhch.practice.easyruledemo.rules.MyRuleTwo;
 import org.jeasy.rules.api.*;
 import org.jeasy.rules.core.DefaultRulesEngine;
+import org.jeasy.rules.core.RuleBuilder;
 
 /**
  * 本地规则启动,无需启动spring容器
@@ -22,6 +23,7 @@ public class LocalRuleApplication {
         RulesEngineParameters parameters = new RulesEngineParameters();
         RulesEngine rulesEngine = new DefaultRulesEngine(parameters);
 
+        // 自定义规则对象
         Rules rules = new Rules();
         rules.register(new MyRuleOne());
         rules.register(new MyRuleTwo());
@@ -37,6 +39,30 @@ public class LocalRuleApplication {
             boolean evaluate = rule.evaluate(facts);
             System.out.println(evaluate);
         }
+
+        // 链式编程
+        Facts lambdaFacts = new Facts();
+        lambdaFacts.put("age", 40);
+        Rule ageRule = new RuleBuilder()
+                .name("age rule")
+                .description("Check if person's age is > 18 and marks the person as adult")
+                .priority(1)
+                .when(item -> {
+                    Integer value = item.get("age");
+                    return value > 18;
+                })
+                .when(item -> {
+                    Integer value = item.get("age");
+                    return value < 30;
+                })
+                .then(item -> System.out.println("then age:" + item.get("age")))
+                .build();
+
+        Rules rules1 = new Rules();
+        rules1.register(ageRule);
+        rulesEngine.fire(rules1, lambdaFacts);
+
+        // 还可以使用脚本/表达式以及配置 yaml 文件, 但是会引入新的语法, 暂不整理
     }
 
 }
